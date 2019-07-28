4eed9dac162481838a400db16ec9d56f46eab0f8
developrfw <developrfw@gmail.com>
2019-07-28 12:55:47 -0600

    little progress

--- assets/app.js
+++ assets/app.js
@@ -12,9 +12,19 @@ function renderButtons(){
   $('#addDogs').on('click', function(){
 
    
-   var dogs = $('#dogs-input').val();
+   var dogs = $('#user-input').val();
    $('#dogsView').append('<button>' + movie + '</button>');
    return false;
 
 });
 
+
+renderButtons();
+
+
+// the URL querying the API ((( put this in an onClick Function)))
+var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=R31pFJRv69lBkkbwiUza4FfSBDl3IWmk&limit=10"
+
+var searchTerm = $("#user-input").val()
+
+ 
