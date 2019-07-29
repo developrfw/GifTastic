fa1eb9905703c51379922eac4101b4560ea35880
developrfw <developrfw@gmail.com>
2019-07-28 21:02:43 -0600

    taking a break. stuck on logging the response when buttons are clicked

--- assets/app.js
+++ assets/app.js
@@ -1,20 +1,33 @@
 $( document ).ready(function() {
     renderButtons(searchArray, 'searchButton', '#buttonsArea');
-    console.log( "ready!" );
 });
-
 
 var searchArray = ["golden retriever", "labrador", "border collie", "great dane", "great pyrenees", "greyhound", "australian shephard", "chihuahua", "dascshund"];
 
-function renderButtons(searchArray, classToAdd, areaToAddTo){
-    $(areaToAddTo).empty();
+
+
+function renderButtons(){
+    $("#buttonsArea").empty(); 
     for(var i=0; i < searchArray.length; i++){
-        var a = $('<button>');
-        a.addClass(classtoAdd);
+        var a = $("<button>");
+        a.addClass("");
         a.attr("data-type", searchArray[i]);
         a.text(searchArray[i]);
-        $(areaToAddTo).append(a);
+        $("#buttonsArea").append(a);
     }
 
 }
+
+
+$(document).on('click', ".searchButton" , function(){
+    var type = $(this).data('type');
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=R31pFJRv69lBkkbwiUza4FfSBDl3IWmk&limit=10"
+    $.ajax({url:queryURL, method: 'GET'})
+    .done(function(response){
+        console.log(response)
+    })
+    
+})
+
+
 
