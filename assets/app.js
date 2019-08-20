e3c13b4b24b0fd39836dd2eba32ea6e33c237be3
developrfw <developrfw@gmail.com>
2019-08-19 18:45:30 -0600

    progress after Jason's help

--- assets/app.js
+++ assets/app.js
@@ -1,49 +1,81 @@
-$( document ).ready(function() {
-    renderButtons(searchArray, 'searchButton', '#buttonsArea');
-});
 
 var searchArray = ["golden retriever", "labrador", "border collie", "great dane", "great pyrenees", "greyhound", "australian shephard", "chihuahua", "dascshund"];
 
+
+
+
+// var gifURL = response.data[g].images.original_still.url;
+//         var gifURLAnimated = response.data[g].images.original.url;
+//         console.log(gifURLAnimated);
+
+//         $( document ).ready(function() {
+//             renderButtons(searchArray, 'searchButton', '#buttonsArea');
+//         });
+        
 
 
 function renderButtons(){
     $("#buttonsArea").empty(); 
     for(var i=0; i < searchArray.length; i++){
         var a = $("<button>");
-        a.addClass("");
+        a.addClass("value-button");
         a.attr("data-type", searchArray[i]);
         a.text(searchArray[i]);
         $("#buttonsArea").append(a);
     }
 
 }
+renderButtons()
 
-
-$(document).on('click', ".searchButton" , function(){
-    var type = $(this).data('type');
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=R31pFJRv69lBkkbwiUza4FfSBDl3IWmk&limit=10"
-    $.ajax({url:queryURL, method: 'GET'})
-    .done(function(response){
-        console.log(response + "This is the response from ajax")
-    })
+$(document).on('click', ".value-button" , function(){
     
+    var type = $(this).attr('data-type');
+    console.log(type);
+    
+    
+   var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=R31pFJRv69lBkkbwiUza4FfSBDl3IWmk&limit=10"
+    
+
+    $.ajax({
+    url: queryURL,
+    method: "GET",
 })
 
 
+  .then(function(response) {
+    console.log("ajax is working");
+      console.log(response);
+    var results = response.data;
+    console.log(results);
 
-function renderButtons(){
-    
-    $("#search-buttons").empty();
-   
-    for (var i = 0; i < searchTerms.length; i++) {
-       
-        a = $("<button>");
-        // add class term-button
-        a.addClass("term-button");
-        // add attribute name
-        a.attr("name", searchTerms[i]);
-        // provide button text
-        a.text(searchTerms[i]);
-        $("#search-buttons").append(a);
+    for (var i = 0; i < results.length; i++) {
+
+        // if (results[i].rating !== "pg" && results[i].rating !== "g") {
+            // var gifDiv = $("<div>");
+            // var rating = results[i].rating;
+
+            // var p = $("<p>").text("Rating: " + rating);
+
+            // var gifImage = $("<img>");
+        //    var gifImage.attr("src", results[i].images.fixed_height.url);
+            
+            // gifDiv.append(p);
+            // gifDiv.append(gifImage);
+
+            // $("#gifDiv").prepend(gifDiv);
+
+            var images = `<div>
+            <img src = ${results[i].images.fixed_height.url}>
+            <p> ${results[i].rating}
+            </div> 
+            `
+            console.log(images);
+
+            $("#gifDiv").prepend(images);
+        // }
     }
-}
+
+    });
+
+})
+  
